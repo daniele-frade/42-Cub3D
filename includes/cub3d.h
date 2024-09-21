6e6d1aec968f04814b958ebb14b3e40ae737d837
Cauê Mendes <caue_1456mendes@outlook.com>
2024-09-21 18:25:22 -0300

    Progress: Draw player

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,25 +6,25 @@
 /*   By: csilva-m <csilva-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 18:22:41 by dfrade            #+#    #+#             */
-/*   Updated: 2024/09/18 19:28:27 by csilva-m         ###   ########.fr       */
+/*   Updated: 2024/09/21 18:10:35 by csilva-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "MLX42/include/MLX42/MLX42.h"
+# include "../libft/headers/get_next_line.h"
 # include "../libft/headers/libft.h"
 # include "../libft/headers/printf.h"
-# include "../libft/headers/get_next_line.h"
-# include <stdio.h>
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdbool.h>
-# include <math.h>
+# include "MLX42/include/MLX42/MLX42.h"
 # include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-enum e_directions
+enum				e_directions
 {
 	NO = 0,
 	SO,
@@ -32,29 +32,20 @@ enum e_directions
 	WE
 };
 
-
-typedef struct s_core {
-	
-	t_map	*map;
-	mlx_image_t* p_image;
-	double p_x;
-	double p_y;
-}	t_core;
-
-typedef struct s_map {
+typedef struct s_map
+{
 	char			**matrix;
 	char			*backup_content;
 	char			**map_matrix;
 	int				p_position_line;
 	int				p_position_col;
-	char			*text_path[5]; // caminho do map
+	char			*text_path[5];
 	char			*f_rgb_str;
 	char			*c_rgb_str;
 	uint32_t		f_rgb_int;
 	uint32_t		c_rgb_int;
-	mlx_texture_t	*textures[4]; // para mlx
-}	t_map;
-
+	mlx_texture_t	*textures[4];
+}					t_map;
 
 typedef struct s_flags
 {
@@ -64,47 +55,57 @@ typedef struct s_flags
 	int				we_flag;
 	int				c_flag;
 	int				f_flag;
-}	t_flags;
+}					t_flags;
+
+typedef struct s_core
+{
+	t_map			*map;
+	mlx_t			*mlx;
+	mlx_image_t		*p_image;
+	mlx_image_t		*m_image;
+	double			p_x;
+	double			p_y;
+}					t_core;
 
 // map validation
-void		arguments_validation(int argc, char *map_file);
-void		map_validation(t_map *map);
-int			file_is_cub(char *file_name);
-int			check_and_get_file(t_map *map, char *file_name);
-int			file_has_all_directions(t_map *map);
-int			directions_has_all_paths(t_map *map);
-int			file_has_valid_rgb(t_map *map);
-int			rgb_has_valid_sintax(char *rgb);
-int			rgb_has_valid_value(char *rgb);
-int			map_has_empty_line(t_map *map);
-int			map_has_only_valid_chars(t_map *map);
-int			map_has_valid_nb_of_players(t_map *map);
-int			map_is_closed_by_walls(t_map *map);
+void				arguments_validation(int argc, char *map_file);
+void				map_validation(t_map *map);
+int					file_is_cub(char *file_name);
+int					check_and_get_file(t_map *map, char *file_name);
+int					file_has_all_directions(t_map *map);
+int					directions_has_all_paths(t_map *map);
+int					file_has_valid_rgb(t_map *map);
+int					rgb_has_valid_sintax(char *rgb);
+int					rgb_has_valid_value(char *rgb);
+int					map_has_empty_line(t_map *map);
+int					map_has_only_valid_chars(t_map *map);
+int					map_has_valid_nb_of_players(t_map *map);
+int					map_is_closed_by_walls(t_map *map);
 
 // map processing
-void		get_map_matrix(t_map *map);
+void				get_map_matrix(t_map *map);
 
-void		get_player_position(t_map *map);
+void				get_player_position(t_map *map);
 
-uint32_t    get_rgb_value(int r, int g, int b, int a);
-void		get_color_from_str(char *rgb_str, uint32_t *rgb_array);
-void		set_rgb_color(t_map *map);
+uint32_t			get_rgb_value(int r, int g, int b, int a);
+void				get_color_from_str(char *rgb_str, uint32_t *rgb_array);
+void				set_rgb_color(t_map *map);
 
-char		*skip_spaces(char *str);
-void		set_textures_path(t_map *map);
-int			load_textures_paths(t_map *map);
-int			is_all_textures_ok(t_map *map);
+char				*skip_spaces(char *str);
+void				set_textures_path(t_map *map);
+int					load_textures_paths(t_map *map);
+int					is_all_textures_ok(t_map *map);
 
 // error
-void		free_and_exit(char **matrix, char *msg);
-void		free_matrix(char **matrix);
-int			error(char *msg);
-void		print_matrix(char **matrix);
-
+void				free_and_exit(char **matrix, char *msg);
+void				free_matrix(char **matrix);
+int					error(char *msg);
+void				print_matrix(char **matrix);
 
 // MLX
-void 		key_data(mlx_key_data_t data, void *map);
-void		print_map2D(t_map *map);
-void		print_square(mlx_image_t *image, int size, int start_line, int start_col, int color);
+void				key_data(mlx_key_data_t data, void *map);
+void				print_map2D(t_map *map);
+void				print_square(mlx_image_t *image, int size, int start_line,
+						int start_col, int color);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,15 +6,15 @@
 /*   By: csilva-m <csilva-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 18:17:57 by dfrade            #+#    #+#             */
-/*   Updated: 2024/09/18 19:33:41 by csilva-m         ###   ########.fr       */
+/*   Updated: 2024/09/21 18:24:48 by csilva-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-#define WINDOW_HEIGHT 1024
+#define WINDOW_HEIGHT 580
 #define WINDOW_WIDTH  1024
-#define CUB_SIZE 24
+#define CUB_SIZE 48
 #define PLAYER_SIZE 24
 
 
@@ -27,9 +27,124 @@ t_core	*get_core(void)
 
 
 
-void	graphics(mlx_t *mlx)
+ void	print_map2D(t_map *map)
+ {
+ 	int	line;
+ 	int col;
+	
+ 	line = 0;
+ 	while (map->map_matrix[line] != NULL)
+ 	{
+ 		col = 0;
+ 		while (map->map_matrix[line][col] != '\0')
+ 		{
+ 			if (map->map_matrix[line][col] == '1')
+ 				print_square(get_core()->m_image, CUB_SIZE, line * CUB_SIZE, col * CUB_SIZE, 0x00000000);
+ 			else if (map->map_matrix[line][col] == '0')
+ 				print_square(get_core()->m_image, CUB_SIZE, line * CUB_SIZE, col * CUB_SIZE, 0x00FFFFFF);
+ 			else if (map->map_matrix[line][col] == 'S')
+ 				print_square(get_core()->m_image, CUB_SIZE, line * CUB_SIZE, col * CUB_SIZE, 0x00FF00FF);
+ 			col++;
+ 		}
+ 		line++;
+ 	}
+ }
+
+ void	reset_image(mlx_image_t *image)
+ {
+ 	int x;
+ 	int y;
+
+ 	y = 0;
+ 	while( y < (int)image->height)
+ 	{
+ 		x = 0;
+ 		while (x < (int)image->width)
+ 		{
+ 			mlx_put_pixel(image, x, y, 0);
+ 			x++;
+ 		}
+ 		y++;
+ 	}
+ }
+
+ void	print_square(mlx_image_t *image, int size, int start_line, int start_col, int color)
+ {
+ 	int x;
+ 	int y;
+
+ 	y = start_line;
+ 	while (y < size + start_line)
+ 	{
+ 		x = start_col;
+ 		while (x < size + start_col)
+ 		{
+ 			mlx_put_pixel(image, x, y, color);
+ 			x++;
+ 		}
+ 		y++;
+ 	}
+ }
+
+
+void	print_map(t_map map)
 {
-	mlx = mlx_init(WINDOW_WIDTH, WINDOW_HEIGHT, "Cub3D", true);
+	ft_printf("\nCompiled successfully\n\n");
+    ft_printf("Map matrix:\n");
+    print_matrix(map.map_matrix);
+    ft_printf("\n");
+    ft_printf("Player position x: %d\n", map.p_position_line);
+    ft_printf("Player position y: %d\n\n", map.p_position_col);
+    ft_printf("Ceiling RGB value: %u\n", map.c_rgb_int);
+    ft_printf("Floor RGB value: %u\n\n", map.f_rgb_int);
+    
+    ft_printf("Texture paths:\n");
+	print_matrix(map.text_path);
+	ft_printf("\n");
+}
+void draw_player(void *param)
+{
+	uint32_t color = *(uint32_t *)param; 
+
+	for (uint32_t i = 0; i < get_core()->p_image->width; ++i)
+	{
+		for (uint32_t y = 0; y < get_core()->p_image->height; ++y)
+		{
+			mlx_put_pixel(get_core()->p_image, i, y, color);
+		}
+	}
+}
+
+void	move_player(void *param)
+{
+	mlx_t* mlx = param;
+
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+	if (mlx_is_key_down(mlx, MLX_KEY_UP))
+		get_core()->p_image->instances[0].y -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
+		get_core()->p_image->instances[0].y += 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
+		get_core()->p_image->instances[0].x -= 5;
+	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
+		get_core()->p_image->instances[0].x += 5;
+}
+
+
+void	init(t_map *map)
+{
+	mlx_t				*mlx;
+	uint32_t color = 0xFF00FF;
+	mlx = mlx_init(WINDOW_WIDTH, WINDOW_HEIGHT, "Cub3d", 1);
+	get_core()->p_image = mlx_new_image(mlx, PLAYER_SIZE, PLAYER_SIZE);
+	get_core()->m_image = mlx_new_image(mlx, CUB_SIZE, CUB_SIZE);
+	mlx_image_to_window(mlx, get_core()->p_image, 0, 0);
+	mlx_image_to_window(mlx, get_core()->m_image, 0, 0);
+	//draw_player
+	mlx_loop_hook(mlx, draw_player, &color);
+	mlx_loop_hook(mlx, move_player, mlx);
+	//print_map2D(map);
 	
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
@@ -39,7 +154,6 @@ void	graphics(mlx_t *mlx)
 int main(int argc, char **argv)
 {
 	t_map				map;
-	mlx_t				*mlx;
 	// inicializa struct map
 	ft_bzero(&map, sizeof(t_map));
 	
@@ -58,23 +172,10 @@ int main(int argc, char **argv)
 		error("Error\nWrong texture path\n");
 	
 	// print map_matrix
-	ft_printf("\nCompiled successfully\n\n");
-    ft_printf("Map matrix:\n");
-    print_matrix(map.map_matrix);
-    ft_printf("\n");
-    ft_printf("Player position x: %d\n", map.p_position_line);
-    ft_printf("Player position y: %d\n\n", map.p_position_col);
-    ft_printf("Ceiling RGB value: %u\n", map.c_rgb_int);
-    ft_printf("Floor RGB value: %u\n\n", map.f_rgb_int);
-    
-    ft_printf("Texture paths:\n");
-	print_matrix(map.text_path);
-	ft_printf("\n");
+	print_map(map);
 	
-
 	
-	graphics(mlx);
-	
+	init(&map);
 	
 	// parte de free da validação do mapa
 	free_matrix(map.matrix);
